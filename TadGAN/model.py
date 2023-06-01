e3cdbc5082792035d6d9307f78d2b9e1f6e24338
melli0505 <dmelli0505@gmail.com>
2023-06-01 16:58:21 +0900

    modified latent dimension

--- TadGAN/model.py
+++ TadGAN/model.py
@@ -19,10 +19,10 @@ class Encoder(nn.Module):
     def __init__(self, encoder_path, signal_shape=100):
         super(Encoder, self).__init__()
         self.signal_shape = signal_shape
-        self.lstm = nn.LSTM(input_size=self.signal_shape, hidden_size=40, num_layers=1, bidirectional=True)
-        # self.lstm = nn.LSTM(input_size=self.signal_shape, hidden_size=20, num_layers=1, bidirectional=True)
-        # self.dense = nn.Linear(in_features=80, out_features=20)
-        self.dense = nn.Linear(in_features=80, out_features=40)
+        self.lstm = nn.LSTM(input_size=self.signal_shape, hidden_size=20, num_layers=1, bidirectional=True)
+        self.dense = nn.Linear(in_features=40, out_features=20)
+        # self.lstm = nn.LSTM(input_size=self.signal_shape, hidden_size=40, num_layers=1, bidirectional=True)
+        # self.dense = nn.Linear(in_features=80, out_features=40)
         self.encoder_path = encoder_path
 
     def forward(self, x, shape=100):
@@ -39,8 +39,8 @@ class Decoder(nn.Module):
     def __init__(self, decoder_path, signal_shape=100):
         super(Decoder, self).__init__()
         self.signal_shape = signal_shape
-        # self.lstm = nn.LSTM(input_size=20, hidden_size=64, num_layers=2, bidirectional=True)
-        self.lstm = nn.LSTM(input_size=40, hidden_size=64, num_layers=2, bidirectional=True)
+        self.lstm = nn.LSTM(input_size=20, hidden_size=64, num_layers=2, bidirectional=True)
+        # self.lstm = nn.LSTM(input_size=40, hidden_size=64, num_layers=2, bidirectional=True)
         self.dense = nn.Linear(in_features=128, out_features=self.signal_shape)
         self.decoder_path = decoder_path
 
@@ -56,10 +56,10 @@ class CriticX(nn.Module):
     def __init__(self, critic_x_path, signal_shape=100):
         super(CriticX, self).__init__()
         self.signal_shape = signal_shape
-        # self.dense1 = nn.Linear(in_features=self.signal_shape, out_features=20)
-        # self.dense2 = nn.Linear(in_features=20, out_features=1)
-        self.dense1 = nn.Linear(in_features=self.signal_shape, out_features=40)
-        self.dense2 = nn.Linear(in_features=40, out_features=1)
+        self.dense1 = nn.Linear(in_features=self.signal_shape, out_features=20)
+        self.dense2 = nn.Linear(in_features=20, out_features=1)
+        # self.dense1 = nn.Linear(in_features=self.signal_shape, out_features=40)
+        # self.dense2 = nn.Linear(in_features=40, out_features=1)
         self.critic_x_path = critic_x_path
 
     def forward(self, x):
@@ -71,8 +71,8 @@ class CriticX(nn.Module):
 class CriticZ(nn.Module):
     def __init__(self, critic_z_path):
         super(CriticZ, self).__init__()
-        # self.dense1 = nn.Linear(in_features=20, out_features=1)
-        self.dense1 = nn.Linear(in_features=40, out_features=1)
+        self.dense1 = nn.Linear(in_features=20, out_features=1)
+        # self.dense1 = nn.Linear(in_features=40, out_features=1)
         self.critic_z_path = critic_z_path
 
     def forward(self, x):
